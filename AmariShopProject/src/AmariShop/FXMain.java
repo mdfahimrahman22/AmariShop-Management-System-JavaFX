eeef92abdeaad4bd4c015f7956dfc9863d209ba0
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-06-26 14:46:48 +0600

    Testing git

--- AmariShopProject/src/AmariShop/FXMain.java
+++ AmariShopProject/src/AmariShop/FXMain.java
@@ -22,7 +22,7 @@ public class FXMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'AmariShop'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
