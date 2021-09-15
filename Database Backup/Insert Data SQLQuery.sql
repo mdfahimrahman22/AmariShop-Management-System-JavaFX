d2c2554c3bb964fac820e5b41c8e8cefff11135d
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-09-15 12:50:40 +0600

    update insert query

--- Database Backup/Insert Data SQLQuery.sql
+++ Database Backup/Insert Data SQLQuery.sql
@@ -12,10 +12,14 @@ values (1,'Dhanmondi Branch','7/2a, Dhanmondi, Dhaka','01615990017')
 select * from Branch
 
 --> Creating UserRole
-insert into UserRole(user_role_title)
-values ('Shop Admin'),('Branch Admin'),('Operator')
+set identity_insert userrole on
+insert into UserRole(UserRoleID,user_role_title)
+values (1,'Shop Admin'),(2,'Branch Admin'),(3,'Operator')
+set identity_insert UserRole off
 
 select * from UserRole
+
+delete from UserRole
 
 --> Creating User
 insert into Users(BranchID,UserRoleID,name,email,password,address,contact)
@@ -24,6 +28,9 @@ values (1,2,'Fahim Rahman','fahimpranto002@gmail.com','123456','2/a Modhubagh, D
 --> Creating User 2
 insert into Users(BranchID,UserRoleID,name,email,password,address)
 values (1,3,'Ifrat Jahan Chowdhury','180204003@aust.edu','123456','AUST, Dhaka')
+
+update users set UserRoleID=1 where userid=1
+update users set UserRoleID=2 where userid=2
 
 select * from Users
 --> 
